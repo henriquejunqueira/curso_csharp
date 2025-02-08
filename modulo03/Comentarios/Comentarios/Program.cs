5f230aa9e5732597c0515cf6daea5ed3ef3871e9
unknown <henriquejunqueira15@gmail.com>
2025-02-08 13:32:08 -0300

    Trabalhando com comentários

---
+++ modulo03/Comentarios/Comentarios/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comentarios
+{
+    internal class Program
+    {
+        // Método principal
+        // Outra linha
+        static void Main(string[] args)
+        {
+            /*
+            Este é um bloco de comentário
+            Posso inserir árias linhas
+            E ainda outra linha
+            */
+
+            Console.WriteLine("Olá mundo!");
+
+            Console.ReadKey();
+        }
+    }
+}
